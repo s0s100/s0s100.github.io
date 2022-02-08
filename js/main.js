c01c14e24b398d1cf386cc97ad199e0a4bbb9bd8
s0s100 <s0s100@mail.ru>
2022-02-08 16:44:26 +0000

    Bottom bar change, added new button, gif file resize

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ jQuery(document).ready(function($) {
         itemsDesktop : [1000,3], //5 items between 1000px and 901px
         itemsDesktopSmall : [900,2], // betweem 900px and 601px
         itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        itemsMobile : true // itemsMobile disabled - inherit from itemsTablet option
         
     });
 
